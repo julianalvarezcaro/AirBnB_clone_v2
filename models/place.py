60061a679def1fc04f288ee893940656f70f390f
Sofiag8 <1943@holbertonschool.com>
2020-12-17 22:17:40 +0000

    place relationship with reviews

--- models/place.py
+++ models/place.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.review import Review
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-
-    # Volver columnas
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    # Crear relacion con Review
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review",
+                               cascade="all, delete",
+                               backref="place")
+    else:
+        @property
+        def reviews(self):
+            reviews_list = []
+            reviews_dict = models.storage.all(Review)
+            for rev_id, obj in reviews_dict.items():
+                if self.id == obj.place_id:
+                    reviews_list.append(obj)
+            return reviews_list
     # Instance of SQLAlchemy Table called place_amenity
-
-    # Condicional entre DBStorage y FileStorage
