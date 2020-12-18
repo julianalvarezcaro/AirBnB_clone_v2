820f5f04d0e121dfb312912f6bbaece9a3b58e31
Sofiag8 <1943@holbertonschool.com>
2020-12-18 00:00:04 +0000

    importing all classes to init

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,13 @@
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
