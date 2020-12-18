00ae73c5f139fb531fc4b9210b3e3d04dbcf1829
Sofiag8 <1943@holbertonschool.com>
2020-12-18 00:00:40 +0000

    creating conection with place and linked table

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Comment"""
-    __tablename__ = 'users'
-
-    # Volver columnas
-    name = ""
-
-    # Crear relacion Many-to-Many place_amenities
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
