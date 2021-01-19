aa012c2c60384bf883cfddc05f1dcda63da6d543
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-19 16:29:39 +0000

    Adding README, task 0 and 1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Simple application of Flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """Returns Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+
+app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+This project implements Flash to be able to handle correctly URL and what to do with them
