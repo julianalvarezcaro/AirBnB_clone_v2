c5fe7a191073b13dc28fbe23b1c1626289dc3930
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-19 17:00:59 +0000

    Completed task 2 and 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Simple application of Flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_HBNB():
+    """Returns Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """Returns Hello HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>')
+def c_isfun(text):
+    """Returns Hello HBNB
+    """
+    string = text.replace('_', ' ')
+    return 'C ' + string
+
+app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Simple application of Flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_HBNB():
+    """Returns Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """Returns Hello HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>')
+def c_isfun(text):
+    """Returns Hello HBNB
+    """
+    string = text.replace('_', ' ')
+    return 'C ' + string
+
+@app.route('/python')
+@app.route('/python/<string:text>')
+def python_iscool(text="is cool"):
+    """Returns Hello HBNB
+    """
+    string = text.replace('_', ' ')
+    return 'Python ' + string
+
+app.run(host="0.0.0.0", port=5000)
